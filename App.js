80330745c0a7ce4530b856e98fe900092b82413e
SHOTABERIASHVILI13 <134447397+SHOTABERIASHVILI13@users.noreply.github.com>
2023-07-05 23:45:35 +0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,52 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+
+
+function App() {
+
+
+  
+
+  return (
+    <div className="App">
+
+      
+    <div className="margins">
+
+        <header>marketprice</header>
+            
+          
+        
+    </div>  
+
+      
+
+        <nav >
+        
+          <div id='topnav' className="topnav">
+            
+           
+
+           
+            <search className='searchbar' id='searchbar'>ძებნა</search>
+            
+            
+          </div>
+        </nav>
+
+      
+   
+
+    <div id='div1' className="div1">
+     იპოვე სასურველი პროდუქცია <br/> საუკეთესო ფასად
+    </div>
+
+  <footer>marketprice<br/>თბილისი,საქართველო</footer>
+
+</div>
+  
+  );
+}
+
+export default App;
